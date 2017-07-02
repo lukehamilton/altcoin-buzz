a440637d33f1ef5d646dce1709d7dcc82f514604
Luke Hamilton <lukehamiltonmail@gmail.com>
2017-07-02 17:47:12 -0400

    tweak domain name

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ CryptoBuzz::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'cryptobuzz.io',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
